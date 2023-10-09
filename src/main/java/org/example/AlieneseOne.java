2be23b63f0bca0a90e6dcc5c4bfac39b0fc5a04d
dryckman2 <dryckman2@huskers.unl.edu>
2023-10-09 08:53:01 -0500

    Implementation of Alienese One translation

--- src/main/java/org/example/AlieneseOne.java
+++ src/main/java/org/example/AlieneseOne.java
@@ -2,10 +2,9 @@ package org.example;
 
 
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -14,19 +13,24 @@ import javafx.scene.layout.StackPane;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class AlieneseOne {
-    private static final Map<Character, Image> symbolSet = new HashMap<>();
+    private static final Map<String, Image> symbolSet = new HashMap<>();
 
-    public static final Set<Character> ENGLISH_SET = Set.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+    public static final List<String> ENGLISH_SET = List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+    public static final List<String> NON_STANDARD_SET = List.of("space");
 
     static {
         //Initialize Code
         try {
-            for (char c : ENGLISH_SET) {
-                symbolSet.put(c, new Image(new FileInputStream("data/AlieneseOne/" + c + ".png")));
+            for (String c : ENGLISH_SET) {
+                symbolSet.put(String.valueOf(c), new Image(new FileInputStream("data/AlieneseOne/" + c + ".png")));
+            }
+            for (String s : NON_STANDARD_SET) {
+                symbolSet.put(s, new Image(new FileInputStream("data/AlieneseOne/" + s + ".png")));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -34,16 +38,25 @@ public class AlieneseOne {
     }
 
 
-    public static ImageView getSymbol(char i) {
+    public static ImageView getSymbol(String i) {
         return getSymbol(i, 30);
     }
 
-    public static ImageView getSymbol(char i, int size) {
+    public static ImageView getSymbol(String i, int size) {
+        if (i.equals(" ")) {
+            i = "space";
+        }
         ImageView view = new ImageView(symbolSet.get(i));
         view.setFitHeight(size * 1.5);
         view.setFitWidth(size);
         return view;
     }
+
+    private static String enteredText;
+
+    private static final HBox displayLine = new HBox();
+    private static final TextField tf = new TextField();
+
 
     public static Node display() {
         StackPane stackPane = new StackPane();
@@ -51,28 +64,55 @@ public class AlieneseOne {
 
         GridPane gridPane = new GridPane();
 
-        for (char i = 'a'; i <= 'z'; i++) {
-            addSymbolButtonAtIndex(i, gridPane, (i - 'a') % 10, (i - 'a') / 10);
+        int i = 0;
+        for (String s : ENGLISH_SET) {
+            addSymbolButtonAtIndex(s, gridPane, i % 10, i / 10);
+            i++;
         }
-        for (char i = '0'; i <= '9'; i++) {
-            addSymbolButtonAtIndex(i, gridPane, (i - '0') % 10, 3);
+        i = 0;
+        for (String s : NON_STANDARD_SET) {
+            addSymbolButtonAtIndex(s, gridPane, i % 10, 4);
+            i++;
         }
 
         vBox.getChildren().add(gridPane);
 
-        vBox.getChildren().add(new Label("this is a test"));
+        enteredText = "Enter Text here";
+        updateDisplayLine();
+        vBox.getChildren().add(displayLine);
+
+        tf.setText(enteredText);
+        tf.setOnAction((event) -> {
+            TextField t = (TextField) event.getSource();
+            enteredText = t.getText();
+            updateDisplayLine();
+        });
+        vBox.getChildren().add(tf);
 
         stackPane.getChildren().add(vBox);
         return stackPane;
     }
 
-    private static void addSymbolButtonAtIndex(char c, GridPane gridPane, int x, int y) {
+    public static void updateDisplayLine() {
+        displayLine.getChildren().clear();
+        for (char c : enteredText.toLowerCase().toCharArray()) {
+            displayLine.getChildren().add(getSymbol(String.valueOf(c), 20));
+        }
+        tf.setText(enteredText);
+    }
+
+    private static void addSymbolButtonAtIndex(String c, GridPane gridPane, int x, int y) {
         Button button = new Button(String.valueOf(c));
         button.setGraphic(AlieneseOne.getSymbol(c));
         button.setContentDisplay(ContentDisplay.TOP);
         button.setOnAction((action) -> {
             Button s = (Button) action.getSource();
-            System.out.println(s.getText());
+            if (s.getText().equals("space")) {
+                enteredText += ' ';
+            } else {
+                enteredText += s.getText();
+            }
+            updateDisplayLine();
         });
         gridPane.add(button, x, y);
     }
